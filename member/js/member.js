1e587b4665dd910245f1551389853b3fed56e92f
jjun0hg <jjun0hg@gmail.com>
2023-02-24 18:13:42 +0900

    회원가입

---
+++ member/js/member.js
@@ -0,0 +1,21 @@
+function select() {
+			document.writeForm.email2.value = document.writeForm.email3.value;
+		}
+		function checkWrite(){
+			
+			document.getElementById("nameDiv").innerText="";
+			document.getElementById("idDiv").innerText="";
+			document.getElementById("pwdDiv").innerText="";
+			
+			if(document.getElementById("name").value == "")
+				document.getElementById("nameDiv").innerText="이름 입력";
+			else if(document.getElementById("id").value == "")
+				document.getElementById("idDiv").innerText="아이디 입력";
+			else if(document.getElementById("pwd").value == "")
+				document.getElementById("pwdDiv").innerText="비밀번호 입력";
+			else if(document.getElementById("pwd").value != document.getElementById("repwd").value)
+				document.getElementById("pwdDiv").innerText="비밀번호가 맞지 않습니다.";
+			else
+				document.writeForm.submit();
+				
+		}
